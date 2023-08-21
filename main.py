647898c164fafe9c99d300769bd686b91612eb90
BaoAL31 <jemboong@gmail.com>
2023-08-21 12:49:55 +1000

    Added perfect-pixel collision and sprites for player and bullets

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
 import random
-
 import pygame
 import time
 
@@ -10,59 +9,76 @@ h = surface.get_height()
 w = surface.get_width()
 color = (255,255,0)
 clock = pygame.time.Clock()
-running = True
-gravity = 0.5
 
-class Projectile():
+# Create a mask from the helicopter png
+
+running = True
+gravity = 0.3
+
+class Projectile(pygame.sprite.Sprite):
     def __init__(self, angle):
-        self.x = w
-        self.y = h/2
+        super().__init__()
         self.angle = angle/100
         self.velocity = 5
+        original_image = pygame.image.load("bullet.png").convert_alpha()
+        self.image = pygame.transform.scale(original_image, (20, 20))
+        self.rect = self.image.get_rect(center=(w, h/2))
+        self.mask = pygame.mask.from_surface(self.image)
+        self.mask_image = self.mask.to_surface()
 
-    def get_pos(self):
-        return self.x, self.y
     def update(self):
-        self.x -= self.velocity * math.cos(self.angle)
-        self.y -= self.velocity * math.sin(self.angle)
+        self.rect.x -= self.velocity * math.cos(self.angle)
+        self.rect.y -= self.velocity * math.sin(self.angle)
 
-class Player():
+        if not (-w/2 < self.rect.x < w and -h < self.rect.y < h):
+            self.kill()
+
+
+class Player(pygame.sprite.Sprite):
     def __init__(self):
-        self.y = h/2
+        super().__init__()
+        original_image = pygame.image.load("helicopter.png").convert_alpha()
+        self.image = pygame.transform.scale(original_image,
+                                            (original_image.get_width() / 3, original_image.get_height() / 3))
+        self.rect = self.image.get_rect(center=(200, h/2))
+        self.mask = pygame.mask.from_surface(self.image)
         # Player's upward and downward velocity
         self.vertical_velocity = 0
 
     def add_velocity(self, num):
-        TERMINAL_VELOCITY = 10
+        TERMINAL_VELOCITY = 8
         if -TERMINAL_VELOCITY <= self.vertical_velocity + num <= TERMINAL_VELOCITY:
             self.vertical_velocity += num
-    def get_y(self):
-        return self.y
+
     def update(self):
-        self.y += self.vertical_velocity
-
-
+        self.rect.y += self.vertical_velocity
 
 if __name__ == '__main__':
     timer = 0
-    increment = 1
+    increment = 2
     going_up = False
     player = Player()
-    projectiles = []
+
+    all_sprites = pygame.sprite.Group()
+    all_sprites.add(player)
+    projectiles = pygame.sprite.Group()
+
     while running is True:
         clock.tick(60)
+        # Remove out-of-bound projectiles
         timer += increment
         player.add_velocity(gravity)
-        surface.fill('black')
-        player.update()
-        pygame.draw.rect(surface, 'green', pygame.Rect(200, player.get_y(), 30, 30))
+        surface.fill('dark grey')
+        all_sprites.draw(surface)
+        all_sprites.update()
+        if pygame.sprite.spritecollide(player, projectiles, False, pygame.sprite.collide_mask):
+            print("Collided!")
         if timer > 5 * 60:
-            projectiles.append(Projectile(random.randrange(-45, 45)))
+            projectile = Projectile(random.randrange(-45, 45))
+            all_sprites.add(projectile)
+            projectiles.add(projectile)
             increment += 0.2
             timer = 0
-        for projectile in projectiles:
-            projectile.update()
-            pygame.draw.circle(surface, 'white', projectile.get_pos(), 10)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -75,8 +91,10 @@ if __name__ == '__main__':
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_UP:
                     going_up = False
+
         if going_up is True:
-            player.add_velocity(-1)
+            player.add_velocity(-0.6)
+
         pygame.display.update()
 
 
