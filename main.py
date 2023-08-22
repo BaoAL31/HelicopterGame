21e7fb5162c24b38bab1ffe1c031babe5657ea30
BaoAL31 <jemboong@gmail.com>
2023-08-22 15:12:11 +1000

    Fixed some minor issues with the hitboxes and added the option to retry after being hit by a bullet.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import math
 import random
+import sys
+
 import pygame
 import time
 
@@ -11,18 +13,20 @@ color = (255,255,0)
 clock = pygame.time.Clock()
 
 # Create a mask from the helicopter png
-
-running = True
 gravity = 0.5
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        original_image = pygame.image.load("helicopter.png").convert_alpha()
-        self.image = pygame.transform.scale(original_image, (80, 140))
-        self.rect = self.image.get_rect(center=(120, h/2))
-        self.mask = pygame.mask.from_surface(self.image)
         # Player's upward and downward velocity
         self.vertical_velocity = 0
+
+        original_image = pygame.image.load("helicopter.png").convert_alpha()
+        self.image = pygame.transform.scale(original_image, (100, 150))
+        self.rect = self.image.get_rect(center=(120, h/2))
+        self.mask = pygame.mask.from_surface(pygame.transform.scale(self.image, (95, 150)))
+        self.mask_image = self.mask.to_surface()
+
 
     def add_velocity(self, num):
         TERMINAL_VELOCITY = 8
@@ -36,31 +40,32 @@ class Player(pygame.sprite.Sprite):
 class Projectile(pygame.sprite.Sprite):
     def __init__(self, angle):
         super().__init__()
-        self.x = w
-        self.y = h/2
+        self.x = w + 20
+        self.y = h/2 + 20
         self.angle = math.radians(angle)
         self.velocity = 5
         original_image = pygame.image.load("bullet.png").convert_alpha()
         self.image = pygame.transform.scale(original_image, (20, 20))
-        self.rect = self.image.get_rect(center=(w, h/2))
+        self.rect = self.image.get_rect()
         self.mask = pygame.mask.from_surface(self.image)
+
 
     def update(self):
         self.x -= self.velocity * math.cos(self.angle)
         self.rect.x = self.x
         self.y -= self.velocity * math.sin(self.angle)
         self.rect.y = self.y
-        if not (0 < self.rect.x < w and 0 < self.rect.y < h):
+        self.mask.get_rect().center = self.rect.center
+        if not (-20 < self.rect.x and -20 < self.rect.y < h):
             # print(f'x: {self.x}, rect.x: {self.rect.x}')
             self.kill()
 
-
-if __name__ == '__main__':
+def run_game():
+    running = True
     timer = 0
     increment = 2
     going_up = False
     player = Player()
-
     all_sprites = pygame.sprite.Group()
     all_sprites.add(player)
     projectiles = pygame.sprite.Group()
@@ -71,12 +76,13 @@ if __name__ == '__main__':
         timer += increment
         player.add_velocity(gravity)
         surface.fill('dark grey')
-        all_sprites.draw(surface)
         all_sprites.update()
+        all_sprites.draw(surface)
         if pygame.sprite.spritecollide(player, projectiles, False, pygame.sprite.collide_mask):
-            print("Collided!")
+            break
         if timer > 5 * 60:
             projectile = Projectile(random.randrange(-25, 25))
+            # projectile = Projectile(0)
             all_sprites.add(projectile)
             projectiles.add(projectile)
             increment += 0.1
@@ -89,17 +95,28 @@ if __name__ == '__main__':
                 if event.key == pygame.K_UP:
                     going_up = True
                 elif event.key == pygame.K_ESCAPE:
-                    running = False
+                    pygame.quit()
+                    return False
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_UP:
                     going_up = False
 
         if going_up is True:
             player.add_velocity(-1)
-
         pygame.display.update()
 
+    time.sleep(0.5)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    return True
+                elif event.key == pygame.K_ESCAPE:
+                    return False
 
+if __name__ == '__main__':
+    while run_game():
+        pass
 
 
 
